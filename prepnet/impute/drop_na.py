c54a2055c38bd3beb38c76fda7c3645158e0a4e9
elda27 <kaz.birdstick@gmail.com>
2020-08-03 20:57:19 +0900

    Fixed bad conversion

--- prepnet/impute/drop_na.py
+++ prepnet/impute/drop_na.py
@@ -6,15 +6,23 @@ class DropNA(FrameConverterBase):
     def __init__(self):
         super().__init__()
         self.mask = None
+        self.original = None
+        self.original_dropped_index = None
 
     def encode(self, df:pd.DataFrame):
+        mask = df.isna().any(axis=1)
+        self.original_dropped_index = df.index[mask]
         if get_config('keep_original'):
-            self.mask = df.isna()
-            self.original = df[mask]
+            self.original = df.loc[mask]
         return df.dropna()
 
     def decode(self, df:pd.DataFrame):
-        if self.mask is not None:
-            df = df.insert(self.mask.index, df.columns, self.original)
+        if self.original is not None:
+            df = df.append(self.original)
+        else:
+            df = df.append(pd.DataFrame(
+                None, index=self.original_dropped_index,
+                columns=df.columns
+            ))
         return df
         
