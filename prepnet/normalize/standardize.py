4c05889b95ffc308bc7c55c4f50496db4b9008d8
elda27 <kaz.birdstick@gmail.com>
2020-08-13 04:03:30 +0900

    Fixed to compute normalized parameter at once

--- prepnet/normalize/standardize.py
+++ prepnet/normalize/standardize.py
@@ -6,9 +6,15 @@ import pandas as pd
 class Standardize(ColumnConverterBase):
     """Standardize to N(0, 1)
     """
+    def __init__(self):
+        super().__init__()
+        self.mean = None
+        self.std = None
+
     def encode(self, xs:pd.Series)->pd.Series:
-        self.mean = xs.mean()
-        self.std = xs.std()
+        if self.mean is None:
+            self.mean = xs.mean()
+            self.std = xs.std()
         return (xs - self.mean) / self.std
 
     def decode(self, xs:pd.Series)->pd.Series:
