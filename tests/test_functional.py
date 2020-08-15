9d2f3fedd6c9d050ca986874286a78f79be1c7a7
elda27 <kaz.birdstick@gmail.com>
2020-08-16 02:22:16 +0900

    Modify new test

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -64,3 +64,82 @@ def test_post_processor():
     reconstruct_df = context.decode(output_df_list)
     pd.testing.assert_frame_equal(reconstruct_df[input_df.columns], input_df)
 
+
+def test_disable_execution():
+    input_df = pd.DataFrame({
+        'col': [1, 2, 3, 4, 5, 6],
+    })
+    expected_original_df = pd.DataFrame({
+        'col': [2, 3, 4, 5, 6, 7],
+    })
+    expected_result_df = pd.DataFrame({
+        'col': [3, 4, 5, 6, 7, 8],
+    })
+
+    plus = lambda xs: xs + 1
+    minus = lambda xs: xs - 1
+
+    context = FunctionalContext()
+    with context.enter():
+        context['col'].convert_lambda(
+            plus, minus
+        )
+    with context.enter('minus'):
+        context['col'].convert_lambda(
+            minus, plus
+        )
+    with context.enter():
+        context['col'].convert_lambda(
+            plus, minus
+        )
+    original_output_df = context.encode(input_df)
+    pd.testing.assert_frame_equal(
+        original_output_df[input_df.columns], expected_original_df
+    )
+    original_reconstruct_df = context.decode(original_output_df)
+    pd.testing.assert_frame_equal(
+        original_reconstruct_df[input_df.columns], input_df
+    )
+    
+    disabled_context = context.disable('minus')
+    result_output_df = disabled_context.encode(input_df)
+    pd.testing.assert_frame_equal(
+        result_output_df[expected_result_df.columns], 
+        expected_result_df
+    )
+    result_reconstruct_df = disabled_context.decode(result_output_df)
+    pd.testing.assert_frame_equal(
+        result_reconstruct_df[input_df.columns], 
+        input_df
+    )
+
+def test_disable_post_processor():
+    input_df = pd.DataFrame({
+        'col': [1, 2, 3, 4, 5, 6],
+    })
+    expected_df = pd.DataFrame({
+        'col': [2, 3, 4, 5, 6, 7],
+    })
+
+    context = FunctionalContext()
+    with context.enter():
+        context.lambda_converter(
+            lambda x: x+1,
+            lambda x: x-1
+        )
+    context.post.lambda_converter(
+        lambda x: x+1,
+        lambda x: x-1
+    )
+    context = context.disable('post-process')
+    output_df = context.encode(input_df)
+    pd.testing.assert_frame_equal(
+        output_df[expected_df.columns], 
+        expected_df
+    )
+
+    reconstruct_df = context.decode(output_df)
+    pd.testing.assert_frame_equal(
+        reconstruct_df[input_df.columns], 
+        input_df
+    )
