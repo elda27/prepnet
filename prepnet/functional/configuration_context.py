057ac4e9b359acae1224d1c8fd0c3ee87db3b104
elda27 <kaz.birdstick@gmail.com>
2020-08-16 02:21:35 +0900

    Modify math transformation

--- prepnet/functional/configuration_context.py
+++ prepnet/functional/configuration_context.py
@@ -4,34 +4,7 @@ from prepnet.core.module import copydoc
 from prepnet.functional.frame_context import FrameContext
 
 from prepnet.functional.function_configuration import FunctionConfiguration
-from prepnet.category.onehot_converter import OnehotConverter
-from prepnet.category.ordinal_converter import OrdinalConverter
-from prepnet.normalize.quantile_normalize import QuantileNormalize
-from prepnet.normalize.standardize import Standardize
-from prepnet.impute.nan_imputer import NanImputer
+from prepnet.functional.column_context import ColumnContext
 
-class ConfigurationContext(FrameContext):
-    @copydoc(OnehotConverter)
-    def onehot(self):
-        self.add_config(OnehotConverter)
-        return self
-
-    @copydoc(OrdinalConverter)
-    def ordinal(self):
-        self.add_config(OrdinalConverter)
-        return self
-
-    @copydoc(Standardize)
-    def standardize(self):
-        self.add_config(Standardize)
-        return self
-
-    @copydoc(QuantileNormalize)
-    def quantile_normalize(self, percentile:float=0.99):
-        self.add_config(QuantileNormalize, percentile)
-        return self
-
-    @copydoc(NanImputer)
-    def nan_impute(self):
-        self.add_config(NanImputer)
-        return self
+class ConfigurationContext(FrameContext, ColumnContext):
+    pass
