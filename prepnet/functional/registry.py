16ec171969a664a2dbc6834126269d80d76010e7
elda27 <kaz.birdstick@gmail.com>
2020-08-31 00:13:59 +0900

    Remove old implements

--- prepnet/functional/registry.py
+++
@@ -1,7 +0,0 @@
-from prepnet.functional.configuration_context import ConfigurationContext
-
-def register_converter(name):
-    def _(klass):
-        return klass
-    return _
-ConfigurationContext.aaa = lambda x, y: x + y
