129279862906586a45474b1a98f0654d599e08d9
elda27 <kaz.birdstick@gmail.com>
2020-08-31 00:14:26 +0900

    Remove old implementation

--- prepnet/core/column_converter_base.py
+++ prepnet/core/column_converter_base.py
@@ -5,12 +5,6 @@ class ColumnConverterBase(metaclass=ABCMeta):
     def __init__(self):
         self.origin = None
 
-    async def encode_async(self, xs:pd.Series):
-        yield self.encode(xs)
-
-    async def decode_async(self, xs:pd.Series):
-        yield self.decode(xs)
-
     @abstractmethod
     def encode(self, xs:pd.Series):
         raise NotImplementedError
