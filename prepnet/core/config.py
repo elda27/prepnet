5766d1640a53aa1df9af63fe8da704d4aac333c5
elda27 <kaz.birdstick@gmail.com>
2020-09-08 22:16:19 +0900

    Fixed no updating configuration

--- prepnet/core/config.py
+++ prepnet/core/config.py
@@ -1,17 +1,23 @@
 from contextlib import contextmanager
 
 config = {
-    'keep_original': True
+    'keep_original': True,
+    'raise_satisfied': True,
 }
 
 @contextmanager
 def config_context(key, value):
+    if key not in config:
+        raise KeyError(key)
     old = config[key]
+    config[key] = value
     yield
     config[key] = old
 
 @contextmanager
 def set_config(key, value):
+    if key not in config:
+        raise KeyError(key)
     config[key] = value
 
 def get_config(key):
